65738a7371aef6447908f9a849f887ce1b08d2d5
Juned Memon <junaid18183@gmail.com>
2022-04-13 18:26:15 +0530

    shipment working

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,9 +1,16 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import Table from "../../components/table/Table";
-import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import "./product.scss";
-
+let data;
+const { products } = require("./fakeProductData");
+data = products
 
 const Product = () => {
   return (
@@ -14,7 +21,42 @@ const Product = () => {
         <div className="title">
         Products
         </div>
-        <Table type="product"/>
+        <TableContainer component={Paper} className="table">
+    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell className="tableCell">Name</TableCell>
+          <TableCell className="tableCell">Description</TableCell>
+          <TableCell className="tableCell">Type</TableCell>
+          {/* <TableCell className="tableCell">Image</TableCell> */}
+          <TableCell className="tableCell">Units</TableCell>
+          <TableCell className="tableCell">Available</TableCell>
+          <TableCell className="tableCell">Supplier</TableCell>
+          {/* <TableCell className="tableCell">Available</TableCell> */}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((row) => (
+          <TableRow key={row.id}>
+            <TableCell className="tableCell">{row.name}</TableCell>
+            <TableCell className="tableCell">{row.desc}</TableCell>
+            <TableCell className="tableCell">{row.type}</TableCell>
+            {/* <TableCell className="tableCell">
+              <div className="cellWrapper">
+                <img src={row.banner} alt="" className="image" />
+                {row.product}
+              </div>
+            </TableCell> */}
+
+            <TableCell className="tableCell">{row.unit}</TableCell>
+            <TableCell className="tableCell">{row.price}</TableCell>
+            <TableCell className="tableCell">{row.suplier}</TableCell>
+            {/* <TableCell className="tableCell">{row.available}</TableCell> */}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
       </div>
     </div>
   );
--- src/pages/shipment/Shipment.jsx
+++ src/pages/shipment/Shipment.jsx
@@ -1,6 +1,17 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import "./shipment.scss";
+
+let data;
+const { shipments } = require("./fakeShipmentData");
+data = shipments
 
 const Shipment = () => {
   return (
@@ -11,6 +22,28 @@ const Shipment = () => {
         <div className="widgets">
         Shipment
         </div>
+        <TableContainer component={Paper} className="table">
+    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell className="tableCell">Shipment ID</TableCell>
+          <TableCell className="tableCell">Manager</TableCell>
+          <TableCell className="tableCell">WareHouse</TableCell>
+          <TableCell className="tableCell">Shipped Date</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((row) => (
+          <TableRow key={row.id}>
+            <TableCell className="tableCell">{row.shipment}</TableCell>
+            <TableCell className="tableCell">{row.manager}</TableCell>
+            <TableCell className="tableCell">{row.warehouse}</TableCell>
+            <TableCell className="tableCell">{row.created_on}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
       </div>
     </div>
   );
