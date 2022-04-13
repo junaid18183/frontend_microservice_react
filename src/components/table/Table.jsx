a6f9d466adb3da2a8938367a17ece42f46ca38f3
Juned Memon <junaid18183@gmail.com>
2022-04-13 14:53:48 +0530

    some progress. nav and side bar working

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -6,47 +6,46 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import axios from "axios";
+
 
 const List = () => {
-  const rows = [
-  ];
-  return (
-    <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell className="tableCell">Tracking ID</TableCell>
-            <TableCell className="tableCell">Product</TableCell>
-            <TableCell className="tableCell">Customer</TableCell>
-            <TableCell className="tableCell">Date</TableCell>
-            <TableCell className="tableCell">Amount</TableCell>
-            <TableCell className="tableCell">Payment Method</TableCell>
-            <TableCell className="tableCell">Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell">
-                <div className="cellWrapper">
-                  <img src={row.img} alt="" className="image" />
-                  {row.product}
-                </div>
-              </TableCell>
-              <TableCell className="tableCell">{row.customer}</TableCell>
-              <TableCell className="tableCell">{row.date}</TableCell>
-              <TableCell className="tableCell">{row.amount}</TableCell>
-              <TableCell className="tableCell">{row.method}</TableCell>
-              <TableCell className="tableCell">
-                <span className={`status ${row.status}`}>{row.status}</span>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  const getVideos = async () => {
+    try {
+      const res = await axios.get("api/products/1");
+    } catch (err) {}
+  };
+  const rows = getVideos();
+  console.log(rows)
+  // return ()
+    // <TableContainer component={Paper} className="table">
+    //   <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    //     <TableHead>
+    //       <TableRow>
+    //         <TableCell className="tableCell">Product</TableCell>
+    //         <TableCell className="tableCell">Status</TableCell>
+    //       </TableRow>
+    //     </TableHead>
+    //     <TableBody>
+    //       {rows.map((row) => (
+    //         <TableRow key={row.id}>
+    //           <TableCell className="tableCell">{row.id}</TableCell>
+    //           <TableCell className="tableCell">
+    //             <div className="cellWrapper">
+    //               <img src={row.img} alt="" className="image" />
+    //               {row.product}
+    //             </div>
+    //           </TableCell>
+    //           <TableCell className="tableCell">{row.name}</TableCell>
+    //           <TableCell className="tableCell">
+    //             <span className={`status ${row.status}`}>{row.status}</span>
+    //           </TableCell>
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+    //   </Table>
+    // </TableContainer>
+  // );
 };
 
 export default List;
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,16 +1,26 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Table from "../../components/table/Table";
+import axios from "axios";
 import "./product.scss";
 
+
 const Product = () => {
+  const [setProducts] = [];
+  const getProducts = async () => {
+    try {
+      const res = await axios.get("api/products/1");
+      setProducts(res.data);
+    } catch (err) {}
+  };
+  const rows = getProducts();
   return (
     <div className="product">
       <Sidebar />
       <div className="productContainer">
         <Navbar />
-        <div className="widgets">
-        Product
-        </div>
+        {rows}
+        {/* <Table /> */}
       </div>
     </div>
   );
